3084da83f86ce2460755006da1d22162a512f074
grachro <grachro@gmail.com>
2015-05-04 21:31:41 +0900

    mavenのマルチプロジェクト化

---
+++ dbviewer-macro-java/src/main/java/com/grachro/dbviewer/script/AllJavaScripts.java
@@ -0,0 +1,18 @@
+package com.grachro.dbviewer.script;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.grachro.dbviewer.Script;
+import com.grachro.dbviewer.ScriptHolder;
+
+@ScriptHolder
+public class AllJavaScripts {
+
+	public Set<Script> getScriptList() {
+		Set<Script> result = new HashSet<Script>();
+		result.add(new SampleScript1());
+		result.add(new SampleScript2());
+		return result;
+	}
+}
---
+++ dbviewer-macro-java/src/main/java/com/grachro/dbviewer/script/SampleScript2.java
@@ -0,0 +1,45 @@
+package com.grachro.dbviewer.script;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.grachro.dbviewer.Command;
+import com.grachro.dbviewer.Database;
+import com.grachro.dbviewer.DefaultScript;
+import com.grachro.dbviewer.ScriptHolder;
+
+@ScriptHolder
+public class SampleScript2 extends DefaultScript {
+
+	public SampleScript2() {
+		super("サンプルスクリプト2");
+	}
+
+	@Override
+	public List<String> getUseParams() {
+		List<String> list = new ArrayList<String>();
+		list.add("userName");
+		return list;
+	}
+
+	@Override
+	public void doScript(List<Database> databaseList, List<Command> commandList, Map<String, String> sqlParams) {
+		Database db = databaseList.get(0);
+
+		// JAMWikiのユーザデータを見る場合の例
+
+		String userName = sqlParams.get("userName");
+
+		Command command1 = new Command("カテゴリ", "select * from  jam_users where USERNAME='" + userName + "'");
+		commandList.add(command1);
+		command1.execute(db);
+
+		String sql2 = "select * from  jam_authorities where username='" + userName + "'";
+		Command command2 = new Command("ユーザの権限", sql2);
+		commandList.add(command2);
+		command2.execute(db);
+
+	}
+
+}
