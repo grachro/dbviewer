887d79ae3937ce400dfab4084ebae21b4464feda
grachro <grachro@gmail.com>
2015-08-04 00:54:34 +0900

    dbviewer-coreを別プロジェクトに移動。mavenの親子関係を削除。

--- dbviewer-core/src/main/java/com/grachro/dbviewer/DbRecord.java
+++
@@ -1,9 +0,0 @@
-package com.grachro.dbviewer;
-
-import java.util.LinkedHashMap;
-
-public class DbRecord extends LinkedHashMap<String, String> {
-
-	private static final long serialVersionUID = 1L;
-
-}
